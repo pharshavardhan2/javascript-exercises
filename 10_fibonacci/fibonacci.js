e409d1af563f44385cc332e413a520d433dea888
Harsha <pharshavardhan2@outlook.com>
2024-01-18 01:44:51 +0530

    solved 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num <= 0 || Number(num) == NaN) return "OOPS";
+  num = Number(num);
+  let fibArr = [1, 1];
+  for (let i = 2; i < num; i++) {
+    fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
+  }
+  return fibArr[num - 1];
 };
 
 // Do not edit below this line
