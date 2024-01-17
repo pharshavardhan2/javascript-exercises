910e7182642ce989a74b0ae9f7f1525881f1f746
Harsha <pharshavardhan2@outlook.com>
2024-01-18 01:33:31 +0530

    solved 09_palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.toLowerCase();
+  let i = 0;
+  let j = str.length - 1;
+  while (i < j) {
+    while (!/^[a-z]$/.test(str[i])) i++;
+    while (!/^[a-z]$/.test(str[j])) j--;
+    if (str[i] !== str[j]) return false;
+    return true;
+  }
 };
 
 // Do not edit below this line
