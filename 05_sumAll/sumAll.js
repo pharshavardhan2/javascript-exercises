3ffdb80e1734828ffec4613d1bd2b1e2c58c025c
Harsha <pharshavardhan2@outlook.com>
2024-01-17 15:23:51 +0530

    solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(lowNum, highNum) {
+  if (lowNum < 0 || highNum < 0 || typeof lowNum != "number" || 
+     typeof highNum != "number") return "ERROR";
+  if (lowNum > highNum) {
+    temp = highNum;
+    highNum = lowNum;
+    lowNum = temp;
+  }
+  let sum = 0;
+  for (let i = lowNum; i <= highNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
