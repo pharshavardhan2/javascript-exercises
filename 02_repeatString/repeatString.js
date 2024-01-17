00464030bd8ac3d74644b030e3151af53598b657
Harsha <pharshavardhan2@outlook.com>
2024-01-17 14:49:09 +0530

    solved 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, repetitionCount) {
+  if (repetitionCount < 0) return 'ERROR';
+  let result = "";
+  for (let i = 0; i < repetitionCount; i++) {
+    result += str;
+  }
+  return result;
 };
 
 // Do not edit below this line
